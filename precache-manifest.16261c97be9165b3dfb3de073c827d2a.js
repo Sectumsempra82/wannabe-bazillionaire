02ec10a79a38da869303f096be1443b40ed669be
unknown <sectumsempra82@gmail.com>
2019-05-07 22:37:32 +0200

    Updates

---
+++ precache-manifest.16261c97be9165b3dfb3de073c827d2a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49402e8a697f445b85b51e2a7c1a58c0",
+    "url": "/wannabe-bazillionaire/index.html"
+  },
+  {
+    "revision": "84e13f80749fa0eadc99",
+    "url": "/wannabe-bazillionaire/static/css/main.c626b463.chunk.css"
+  },
+  {
+    "revision": "4c498a12ab1d025b908b",
+    "url": "/wannabe-bazillionaire/static/js/2.c4aa1ba2.chunk.js"
+  },
+  {
+    "revision": "84e13f80749fa0eadc99",
+    "url": "/wannabe-bazillionaire/static/js/main.5f0dd7c1.chunk.js"
+  },
+  {
+    "revision": "cfb8c8061f8f391dd965",
+    "url": "/wannabe-bazillionaire/static/js/runtime~main.a9d17174.js"
+  },
+  {
+    "revision": "4c1df52979abeb2a30c457b169a9048a",
+    "url": "/wannabe-bazillionaire/static/media/bg-large.4c1df529.jpg"
+  },
+  {
+    "revision": "bcd9ae6b6848d47e0af64aadf18552d0",
+    "url": "/wannabe-bazillionaire/static/media/bg-small.bcd9ae6b.jpg"
+  },
+  {
+    "revision": "5f11feebcb6a8469f1a368266b9bfb94",
+    "url": "/wannabe-bazillionaire/static/media/bg-xs.5f11feeb.jpg"
+  },
+  {
+    "revision": "06cb133dd52198332e539fdaf3fbd0fd",
+    "url": "/wannabe-bazillionaire/static/media/logo.06cb133d.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wannabe-bazillionaire/precache-manifest.16261c97be9165b3dfb3de073c827d2a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wannabe-bazillionaire/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
